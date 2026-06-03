92788253f603fbaa49b07085f61971386900908e
selfnesss <sejdzuro0@gmail.com>
2026-06-03 20:07:07 +0300

    Allow production domain in Vite dev server

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["ideadiagram.ru", "www.ideadiagram.ru"],
     proxy: {
       "/api": "http://127.0.0.1:8787"
     }
